730c8ce4149760e400d2df7d149434e94666c46c
0xpranjl <pranjal.maurya402@gmail.com>
2022-12-30 16:53:06 +0530

    full n final

---
+++ screens/Coupons.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react';
+import { Text, View, StyleSheet, Button } from 'react-native';
+import { BarCodeScanner } from 'expo-barcode-scanner';
+function Coupons() {
+    
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+       <Text>welcome to Coupons tab</Text>
+      </View>
+    );
+  }
+export default Coupons;
